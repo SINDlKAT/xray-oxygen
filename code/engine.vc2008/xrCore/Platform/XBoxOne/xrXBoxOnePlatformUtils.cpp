9f54803e984037e3f66d322a7342b3ec5e8708e1
ForserX <sv3nk@yandex.ru>
2019-07-31 14:04:18 +0300

    * PFM: Impl XBox One platform code

---
+++ code/engine.vc2008/xrCore/Platform/XBoxOne/xrXBoxOnePlatformUtils.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////
+// Desc		: XBox One miscellaneous functions
+// Author	: ForserX
+//////////////////////////////////////////////////////////////
+// Oxygen Engine 2016-2019
+//////////////////////////////////////////////////////////////
+#include "stdafx.h"
+#include "xrXBoxOnePlatformUtils.h"
+
+XRCORE_API xrXBoxOnePlatformUtils PlatformUtils;
+
+LPCSTR xrXBoxOnePlatformUtils::GetName()
+{
+	return "XBox-One";
+}
+
+int xrXBoxOnePlatformUtils::ShowCursor(bool bShowCursor)
+{
+	return ::ShowCursor(bShowCursor);
+}
+
+const char* xrXBoxOnePlatformUtils::GetRender()
+{
+	return "xrRender_RX.dll";
+}
